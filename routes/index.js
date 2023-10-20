fa81e79e42d2759a6a5a1c25921f19de2662522d
FirstPasit <giccer11@gmail.com>
2023-10-20 10:54:17 +0700

    store module

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const store = require("./store");
+store.init();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +12,11 @@ router.get('/hello', function(req, res) {
   //res.send("Hello world!"); //send respond to client
   res.setHeader("Content-Type","application/json");
   res.send({"res_text":"Hello World!"});
+});
+
+//see in store module
+router.get('/items', function(req, res,) {
+  res.send({items: store.getAllItems()});
 });
 
 module.exports = router;
